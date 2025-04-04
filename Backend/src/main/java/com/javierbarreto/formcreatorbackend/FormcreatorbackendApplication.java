32c71a20a426e12f9335fb94134c91c8c5b87544
Javier Barreto <javierbarretocp@gmail.com>
2025-04-04 11:17:59 -0600

    adding backend

---
+++ Backend/src/main/java/com/javierbarreto/formcreatorbackend/FormcreatorbackendApplication.java
@@ -0,0 +1,13 @@
+package com.javierbarreto.formcreatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormcreatorbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormcreatorbackendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/javierbarreto/formcreatorbackend/FormcreatorbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javierbarreto.formcreatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormcreatorbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
